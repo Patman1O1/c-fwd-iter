00b429f6429698b2970a7c8f2c404a7dcf6807b7
Patman1O1 <patrickthomas101x@gmail.com>
2026-04-02 17:42:35 -0500

    Implement struct fwd_iter

--- include/fwd-iter/fwd_iter.h
+++ include/fwd-iter/fwd_iter.h
@@ -1,13 +1,20 @@
-#ifndef C_FWD_ITER_H
-#define C_FWD_ITER_H
+#ifndef FWD_ITER_H
+#define FWD_ITER_H
+
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif // #ifdef __cplusplus
 
+struct fwd_iter {
+    void* (*next)(void* struct_p);
+    bool (*has_next)(const void* struct_p);
+};
+
 #ifdef __cplusplus
 }
 #endif // #ifdef __cplusplus
 
-#endif // #ifndef C_FWD_ITER_H
+#endif // #ifndef FWD_ITER_H
 
